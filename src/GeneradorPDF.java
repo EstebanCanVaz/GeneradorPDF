1777dca9aa918b4197c7fb1f0aa82d4b12dfdbd4
Maur2012 <mau.emi2000@gmail.com>
2025-08-29 16:49:26 -0600

    add GeneradorPDF y LecturaCSV

---
+++ src/GeneradorPDF.java
@@ -0,0 +1,5 @@
+public class GeneradorPDF {
+    public void generar(Controlador controlador){
+
+    }
+}
---
+++ src/LecturaCSV.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.Map;
+
+public class LecturaCSV {
+    public LecturaCSV(String ruta) {
+
+    }
+
+    public List<String> getIdentificadores(){
+        return null;
+    }
+
+    public List<Map<String, String>> getRegistros(){
+        return null;
+    }
+
+}
